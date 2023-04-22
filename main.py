1aa926c20cb8bfab761c2427f722d41c70d1c226
JHLee <leejunhee1219@snu.ac.kr>
2023-04-22 21:01:09 +0900

    http

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ cors = CORS(app)
 
 @app.route('/api/login', methods=['POST'])
 def login():
-
-    print(request)
     CODE = request.form['CODE']
     response = {'result': CODE, 'message': CODE}
     
-    # 여기에 로그인 로직을 구현합니다.
-    # 성공적으로 로그인을 했다면, 아래와 같이 JSON 응답을 반환합니다.
+
+
+
     return jsonify(response)
 
 if __name__ == '__main__':
